d6206ec0a603d1b6cd723ced9a81491a03f7d239
Abdelrhman Allam <abdelrhman.allam@gmail.com>
2020-06-29 22:06:29 +0200

    Initial commit

---
+++ app/graphql/myapp_schema.rb
@@ -0,0 +1,5 @@
+class MyappSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+  
+end
---
+++ app/graphql/types/mutation_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
+    field :create_link, mutation: Mutations::CreateLink
+  end
+  
+end
